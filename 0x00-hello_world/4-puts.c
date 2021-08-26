3e6904f81d527457a828ef12c44277107d73f998
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-26 00:10:25 -0700

    put msg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File 4-puts.c
+ * Auth Abdurahman Mamu <abdimahmudhaji227@gmail.cm>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}
