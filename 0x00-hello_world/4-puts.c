0e9bee47716287cb51c65a0d70d3392783c01c2c
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-26 14:29:45 -0700

    putting str using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
-/*
- * File 4-puts.c
- * Auth Abdurahman Mamu <abdimahmudhaji227@gmail.cm>
- */
-
 #include <stdio.h>
-
 /**
- * main - prints "programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- *Return: Always 0.
+ * main - Prints string
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
