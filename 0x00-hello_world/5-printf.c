5248eeea704767e0c63f8d1b582ccb2d986675af
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-26 13:32:09 -0700

    printing str

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints string
- * Desciption: Using printf
+ * main - Print string
+ * Description: Using printf
  * Return: 0
  */
 int main(void)
 {
-printf("%s", "with proper grammmar, but the outcome is a piece of art,\n");
-
-return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
