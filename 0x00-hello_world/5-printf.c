b8d6bdd0710c4d8b2cecb483986b4261ae30f212
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-26 04:57:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ *File 5-printf.c
+ *Auth Abdurahman Mamu
+ */
+#include <stdio.h>
+/**
+ *main - prints "with proper grammar, but the outcome is 
+ *               a piece of art," followed by a new line.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+  printf("with proper grammmar, but the outcome is a piece of art,\n");
+
+  return (0);
+}
