36db382715d66f6ae5a94249b50fcacdce453ea4
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-27 07:34:04 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 'A';
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
