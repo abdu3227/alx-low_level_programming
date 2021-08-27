d3a92f5af8c6c40b2b5ade1714e837acec847c62
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-27 08:24:45 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entiry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 10;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
