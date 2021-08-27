f644b55e085478efc0b3a98dac08a4835702d3fd
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-08-27 11:37:02 -0700

    puts str

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints string
- * Description: prints "\"Programming is like building a multilingual puzzle"
+ * main - Entry point
+ *
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
