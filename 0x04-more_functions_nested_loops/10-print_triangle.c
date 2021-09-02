08072a47c3cb6dd583417fc81b7d79e019d534c5
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-09-02 07:57:07 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: integer
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	x = 1;
+	while (x <= size && size > 0)
+	{
+		y = 0;
+		while (y < size - x)
+		{
+			_putchar(' ');
+			y++;
+		}
+		y = 0;
+		while (y < x)
+		{
+			_putchar('#');
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+	if (x == 1)
+		_putchar('\n');
+}
