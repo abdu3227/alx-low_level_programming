f39acc27f725569d961b0cf610966d9a533892e0
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-09-02 04:44:24 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiple twu num
+ * @num1: first num
+ * @num2: second num
+ * Return: product
+ */
+
+int mul(int num1, int num2)
+{
+	int product;
+
+	product = num1 * num2;
+	return (product);
+}
