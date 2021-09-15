34fcb2d0676157729d48f0e31945fac338e19c8f
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-09-15 12:49:38 -0700

    string compare

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two string and return 1 if they are identical
+ * @str1: string 1
+ * @str2: string 2
+ * Return: 1 or 0
+ */
+int wildcmp(char *str1, char *str2)
+{
+	if (*str1 == '\0')
+	{
+		if (*str2 != '\0' && *str2 == '*')
+			return (wildcmp(str1, str2 + 1));
+		return (*str2 == '\0');
+	}
+	if (*str2 == '*')
+		return (wildcmp(str1 + 1, str2) || wildcmp(str1, str2 + 1));
+	else if (*str1 == *str2)
+		return (wildcmp(str1 + 1, str2 + 1));
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -19,6 +19,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 /* is_palindrome */
 int is_palindrome(char *s);
+/* wildcmp */
+int wildcmp(char *s1, char *s2);
 
 
 
