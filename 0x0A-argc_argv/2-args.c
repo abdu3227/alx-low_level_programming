c30dfd55d489fdc6fcbcc45fbcad738c2f689bb6
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-09-17 06:17:18 -0700

    num of args passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arg passed to it
+ * @argc: num of args
+ * @argv: array args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
