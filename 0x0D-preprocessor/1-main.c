a859c0ab66b5eb8f202ad817fbde829266f94903
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-09-28 11:35:53 -0700

    pi

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "1-pi.h"
+#include "1-pi.h"
+/**
+ * main - checks the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
