ceccb5acdd7f9d0ddac1284755f261db1d253911
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-10-08 11:46:12 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@ void Hare_n_Tortoise(void)__attribute__((constructor));
  */
 void Hare_n_Tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
