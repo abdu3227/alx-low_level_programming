f598c18f28b169f4e6f0d981e2fb76557e79ab90
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-10-08 11:18:41 -0700

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void Hare_n_Tortoise(void)__attribute__((constructor));
+
+/**
+ * Hare_n_Tortoise - prints text
+ */
+void Hare_n_Tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
