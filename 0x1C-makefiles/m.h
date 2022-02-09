acca510f6f50cade621e43801da0256d1a81e24d
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2022-02-09 04:39:33 -0800

    alltasks

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,9 @@
+#ifndef __M_H__
+#define __M_H__
+
+#include <stdio.h>
+
+void print(void);
+
+#endif
+
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,14 @@
+#include "m.h"
+/**
+ *main - main program
+ *Return: 0
+ *
+ */
+int main(void)
+{
+	print();
+	return (0);
+}
+/* abdu */
+
+/* School */
